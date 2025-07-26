a8484c68ac2ecdc00b7cf18574522b54c6b23bbc
ahmedGHANIM327 <ahmedghanim902@gmail.com>
2025-07-26 16:12:39 +0200

    feat: add logout button to navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,15 +1,24 @@
 import React from 'react';
 import {Link} from "react-router";
+import {usePuterStore} from "~/lib/puter";
 
 export const Navbar = () => {
+
+    const { auth } = usePuterStore();
+
     return (
         <nav className="navbar">
             <Link to="/">
                 <p className="text-2xl font-bold text-gradient">RESUMIND</p>
             </Link>
-            <Link to="/upload" className="primary-button w-fit">
-                Upload Resume
-            </Link>
+            <div className={'flex items-center justify-between gap-2'}>
+                <Link to="/upload" className="primary-button min-w-[155px]">
+                    Upload Resume
+                </Link>
+                <button className={'primary-button'} onClick={auth.signOut}>
+                    <p>Log Out</p>
+                </button>
+            </div>
         </nav>
     );
 };
